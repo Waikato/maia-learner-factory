18131f235b685978b4d15048fa4c6e3694fad306
csterling <coreytsterling@gmail.com>
2021-07-29 14:57:53 +1200

    Initial commit of code-base from old MOANA repos.

---
+++ src/māia/ml/learner/factory/ConfigurableLearnerFactory.kt
@@ -0,0 +1,35 @@
+package māia.ml.learner.factory
+
+/*
+ * TODO
+ *
+ * TODO: Move out of core package so that learner package doesn't depend on
+ *       configuration package.
+ */
+import māia.configure.Configurable
+import māia.configure.Configuration
+import māia.ml.learner.Learner
+
+/**
+ * Base class for factories which can instantiate learners from a configuration.
+ *
+ * @param block
+ *          An initialiser block for the factory's configuration.
+ * @param L
+ *          The type of learner produced by this factory.
+ * @param C
+ *          The type of configuration of the factory.
+ */
+abstract class ConfigurableLearnerFactory<L : Learner<*>, out C : Configuration>(
+        block : C.() -> Unit = {}
+) : Configurable<C>(block) {
+
+    /**
+     * Instantiates a new learner from this factory's configuration.
+     *
+     * @return
+     *          The learner instance.
+     */
+    abstract fun create() : L
+
+}
